333fce8ff03b97d3245af6491bbbdf5cd7a30807
spacedome <spacedome@users.noreply.github.com>
2025-12-21 17:18:49 -0500

    add steam

--- configuration.nix
+++ configuration.nix
@@ -157,11 +157,12 @@
     kdePackages.bluedevil
     mullvad-vpn
   ];
-  # programs.steam = {
-  #   enable = true;
-  #   remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-  #   dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-  # };
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
